e6853ca5540a8a279fbcebb5b31a2e7b2b01de73
Muhammet Fatih Bingöl <118675968+fthbng77@users.noreply.github.com>
2024-02-14 21:06:01 +0300

    Update AppTam-ekran.js

--- src/AppTam-ekran.js
+++ src/AppTam-ekran.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import ROSLIB from 'roslib';
 import DroneData from './DroneData';
+import DroneControl from './DroneControl';
 import MyMapComponent from './MyMapComponent';
 import { Link } from 'react-router-dom';
 
 function App() {
-    // Ekran boyutlarını state olarak tutmak için useState kullanılıyor.
+    const [imgSrc, setImgSrc] = useState(null);
     const [dimensions, setDimensions] = useState({
         height: window.innerHeight,
         width: window.innerWidth,
     });
-
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
     useEffect(() => {
-        // Pencere boyutu değiştiğinde çalışacak fonksiyon
         function handleResize() {
             setDimensions({
                 height: window.innerHeight,
@@ -57,33 +57,47 @@ function App() {
             console.log('Received message on ' + topic.name + ': ', message);
     
             if (message.data) {
-                //const imageUrl = `data:image/jpg;base64,${message.data}`;
-                //setImgSrc(imageUrl);
+                const imageUrl = `data:image/jpg;base64,${message.data}`;
+                setImgSrc(imageUrl);
             }
         });
 
         return () => {
-            window.removeEventListener('resize', handleResize); // Dinleyiciyi kaldır
-            ros.close(); // ROS bağlantısını kapat
+            window.removeEventListener('resize', handleResize);
+            ros.close();
         };
     }, []);
 
+    const toggleMenu = () => {
+        setIsMenuVisible(!isMenuVisible);
+    };
+
     return (
         <div style={{ height: dimensions.height, width: dimensions.width, position: 'relative' }}>
-            {/* Navigasyon Bar */}
-            <nav style={{ position: 'absolute', top: 500, left: 0, right: 0, zIndex: 3 }}>
-                <ul>
-                    <li><Link to="/app">Home</Link></li>
-                    <li><Link to="/ros-image">ROS Image</Link></li>
-                </ul>
-            </nav>
+            {/* Menü Simgesi */}
+            <img src="/menu.svg" alt="Menu" onClick={toggleMenu} style={{ cursor: 'pointer', position: 'absolute', top: 0, left: 0, zIndex: 4 }}/>
 
-            {/* Drone Verileri - Uygulamanın En Üstünde */}
+            {/* Koşullu Menü Renderı */}
+            {isMenuVisible && (
+                <nav style={{ position: 'absolute', top: 50, left: 0, right: 0, zIndex: 3 }}>
+                    <ul>
+                        <li><Link to="/app">Home</Link></li>
+                        <li><Link to="/ros-image">ROS Image</Link></li>
+                    </ul>
+                </nav>
+            )}
+
+            <div style={{ position: 'absolute', bottom: 0, left: 0, zIndex: 3 }}>
+                {imgSrc && <img src={imgSrc} alt="From ROS" style={{ width: '70%', height: 'auto', maxHeight: '100%' }} />}
+            </div>
+            <div style={{ position: 'absolute', bottom:'0%', right: 0,backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '35px', borderRadius: '10px', zIndex: 3 }}>
+                    <DroneControl />
+            </div>
+
             <div style={{ position: 'absolute', top: -40, left: 0, right: 0, zIndex: 3 }}>
                 <DroneData />
             </div>
 
-            {/* MyMapComponent burada ekranın tamamını kaplayacak şekilde yerleştiriliyor. */}
             <div style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, zIndex: 2 }}>
                 <MyMapComponent />
             </div>
