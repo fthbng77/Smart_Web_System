9eb886ec2161ff889b095ad9113e75dd46793488
kayranecatikara <60383469+kayranecatikara@users.noreply.github.com>
2024-05-16 21:49:36 +0300

    Compass.js

--- src/Compass.js
+++ src/Compass.js
@@ -1,54 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import ROSLIB from 'roslib';
-import compassImage from './compass.png';
 
 const Compass = () => {
   const [heading, setHeading] = useState(0);
 
+  useEffect(() => {
+    const ros = new ROSLIB.Ros({
+      url: 'ws://localhost:9090' // ROS bridge sunucunuzun adresi
+    });
 
-    useEffect(() => {
-        const ros = new ROSLIB.Ros({
-            url: 'ws://localhost:9090' // ROS bridge sunucunuzun adresi
-        });
+    ros.on('connection', () => {
+      console.log('Connected to ROS bridge.');
+    });
 
-        ros.on('connection', () => {
-            console.log('Connected to ROS bridge.');
-        });
+    ros.on('error', (error) => {
+      console.error('Error connecting to ROS bridge: ', error);
+    });
 
-        ros.on('error', (error) => {
-            console.error('Error connecting to ROS bridge: ', error);
-        });
+    ros.on('close', () => {
+      console.log('Connection to ROS bridge closed.');
+    });
 
-        ros.on('close', () => {
-            console.log('Connection to ROS bridge closed.');
-        });
+    const compassTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/mavros/global_position/compass_hdg',
+      messageType: 'std_msgs/Float64'
+    });
 
-        const compassTopic = new ROSLIB.Topic({
-            ros: ros,
-            name: '/mavros/global_position/compass_hdg',
-            messageType: 'std_msgs/Float64'
-        });
+    compassTopic.subscribe((message) => {
+      setHeading(message.data);
+    });
 
-        compassTopic.subscribe((message) => {
-            setHeading(message.data);
-        });
+    return () => {
+      compassTopic.unsubscribe();
+      ros.close();
+    };
+  }, []);
 
-        return () => {
-            compassTopic.unsubscribe();
-            ros.close();
-        };
-    }, []);
-    
   const compassStyle = {
     transform: `rotate(${heading}deg)`,
-    width: '150px',
-    height: '150px',
-    transition: 'transform 0.5s ease-in-out',
+    width: '125px', // Pusula görüntüsünün genişliğini ayarlayabilirsiniz
+    height: '125px', // Pusula görüntüsünün yüksekliğini ayarlayabilirsiniz
+    transition: 'transform 0.5s ease-in-out', // Döndürme animasyonu ekleyebilirsiniz
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <img src={compassImage} alt="Compass" style={compassStyle} />
+      <img src={`${process.env.PUBLIC_URL}/compass.png`} alt="Compass" style={compassStyle} />
     </div>
   );
 };
